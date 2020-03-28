21a1aee621f04031facf027be8c1f01e3f910a08
Milen Gardev <milenford@gmail.com>
2020-03-28 22:16:08 +0100

    Adds pm2 watch setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,13 @@
 module.exports = {
     apps : [{
       name: "app",
+      watch: ["public", "src"],
+      watch_delay: 1000,
       script: "ts-node ./src/index.ts",
+      ignore_watch : ["node_modules"],
+      watch_options: {
+        "followSymlinks": false
+      },
       env: {
         NODE_ENV: "development",
       },
