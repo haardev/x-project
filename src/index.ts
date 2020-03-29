dc53ff6c9bae613200ceb60c280ebbc9ac6869f9
Milen Gardev <milenford@gmail.com>
2020-03-29 18:14:30 +0200

    Adds socket io

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,38 @@
-import express from 'express';
+import express, { Response, Request } from 'express';
 import path from 'path';
+import http from 'http';
 import cookieParser from 'cookie-parser';
 import conversations from './routes/conversations';
+import socketio from 'socket.io';
+import { SocketIoEvent, SocketIoEmitMessage } from './types/SocketIoTypes';
 
 const app = express();
 const port =  3000 || process.env.PORT;
 
+const server = http.createServer(app);
+
 app.use(express.json());
 app.use(cookieParser());
+
+// Set static path
+app.use(express.static(path.join(__dirname, '../public')));
 
 //Routes
 app.use('/conversations', conversations);
 
+const io = socketio(server);
+
 //Default index
-app.get('*', (req, res) => {
+app.get('*', (req:Request, res:Response) => {
    res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
  });
 
-app.listen(port, () => console.log(`The server has started on port: ${port}`));
+ io.on(SocketIoEvent.CONNECTION, socket => {
+    socket.emit(SocketIoEmitMessage.MESSAGE, 'You are connected');
+
+    socket.on(SocketIoEvent.DISCONNECT, () => {
+       io.emit(SocketIoEmitMessage.MESSAGE, 'Leave the chat room');
+    });
+ })
+
+server.listen(port, () => console.log(`The server has started on port: ${port}`));
---
+++ src/types/SocketIoTypes.ts
@@ -0,0 +1,8 @@
+export enum SocketIoEmitMessage {
+    MESSAGE = 'message',
+}
+
+export enum SocketIoEvent {
+    CONNECTION = 'connection',
+    DISCONNECT = 'disconnect'
+}
